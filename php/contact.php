2f7b4b3fc4fec3deb36b7e62c79f398bd1a415b5
Nicolas Gillespie <nicolas.gillespie@gmail.com>
2018-03-13 05:58:43 -0700

    ajustement formulaire send

--- php/contact.php
+++ php/contact.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $firstname  = $_REQUEST["firstname"];
 $lastname  = $_REQUEST["lastname"];
 $organisation  = $_REQUEST["organisation"];
@@ -6,7 +6,11 @@ $telephone  = $_REQUEST["telephone"];
 $email = $_REQUEST["email"];
 $subject   = $_REQUEST["subject"];
 $msg   = $_REQUEST["msg"];
-$to    = "j@joseebarrette.com"; // ENTER YOUR EMAIL ADDRESS
+$to    = "nicolas.gillespie@gmail.com"; // ENTER YOUR EMAIL ADDRESS
+// $to    = "j@joseebarrette.com"; // ENTER YOUR EMAIL ADDRESS
+echo $firstname;
+echo $lastname;
+echo $email;
 if (isset($email) && isset($name) && isset($msg)) {
     $email_subject = "$firstname $lastname vous a envoyé un message au sujet de SITL Paris"; // ENTER YOUR EMAIL SUBJECT
 		$headers = "MIME-Version: 1.0" . "\r\n";
@@ -14,7 +18,7 @@ $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$firstname." ".$lastname." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
 $msg     = "De: $firstname $lastname<br/> Compagnie: $organisation <br/>Téléphone: $telephone<br/>
  Courriel: $email";
-	
+
    $mail =  mail($to, $email_subject, $msg, $headers);
   if($mail)
 	{
